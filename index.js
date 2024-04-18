c17584a2d34e316788344c63f1d35885c58e168e
unknown <vismodeb2000@gmail.com>
2024-04-18 12:05:26 +0600

    project created

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Connected to MongoDB!✅");
+    console.log("Connected to MongoDB!!✅");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
