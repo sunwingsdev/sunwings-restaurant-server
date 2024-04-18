b24f815d05c26fe25b3e73e41a3ab95337e63dac
unknown <vismodeb2000@gmail.com>
2024-04-18 12:15:12 +0600

    created branch

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
+  serverApi: { 
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
