3d77e8e4bb33535e425fb7980e2a68f22df7da1f
simanto97 <saimumsimanto97@gmail.com>
2024-04-22 12:35:50 +0600

    delete a item added

--- apis/itemApi/itemApi.js
+++ apis/itemApi/itemApi.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 
 const itemApi = (itemCollection) => {
   const itemRouter = express.Router();
@@ -6,7 +7,6 @@ const itemApi = (itemCollection) => {
   //   add item
   itemRouter.post("/", async (req, res) => {
     const item = req.body;
-    console.log(req.body);
     const result = await itemCollection.insertOne(item);
     res.send(result);
   });
@@ -14,6 +14,18 @@ const itemApi = (itemCollection) => {
   // get items
   itemRouter.get("/", async (req, res) => {
     const result = await itemCollection.find().toArray();
+    res.send(result);
+  });
+
+  // delete a item
+  itemRouter.delete("/:id", async (req, res) => {
+    console.log(req.params.id);
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Invalid id format" });
+    }
+    const query = { _id: new ObjectId(id) };
+    const result = await itemCollection.deleteOne(query);
     res.send(result);
   });
 
