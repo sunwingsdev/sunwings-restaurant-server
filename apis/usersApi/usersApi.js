39396c8a9e989bdc32e4bc726fa01a7643b8a68f
simanto97 <saimumsimanto97@gmail.com>
2024-04-28 16:38:56 +0600

    user api modification

--- apis/usersApi/usersApi.js
+++ apis/usersApi/usersApi.js
@@ -6,7 +6,30 @@ const usersApi = (usersCollection) => {
   //   add user
   userRouter.post("/", async (req, res) => {
     const userInfo = req.body;
+    userInfo.createdAt = new Date();
     const result = await usersCollection.insertOne(userInfo);
+    res.send(result);
+  });
+
+  // get all user
+  userRouter.get("/", async (req, res) => {
+    const result = await usersCollection.find().toArray();
+    res.send(result);
+  });
+
+  // get a single user
+  userRouter.get("/:uid", async (req, res) => {
+    const uid = req.params.uid;
+    const query = { uid: uid };
+    const result = await usersCollection.findOne(query);
+    res.send(result);
+  });
+
+  // delete a user
+  userRouter.delete("/:uid", async (req, res) => {
+    const uid = req.params.uid;
+    const query = { uid: uid };
+    const result = await usersCollection.deleteOne(query);
     res.send(result);
   });
 
